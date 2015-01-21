28f58fa35a6179533f55c16e3a3333d675609621
Tom MacWright <tom@macwright.org>
2015-01-21 12:12:14 -0800

    Fix point form

--- index.js
+++ index.js
@@ -13,8 +13,8 @@
  * @param {String} [units=kilometers] can be degrees, radians, miles, or kilometers
  * @return {Number} distance between the two points
  * @example
- * var point1 = turf.point(-75.343, 39.984);
- * var point2 = turf.point(-75.534, 39.123);
+ * var point1 = turf.point([-75.343, 39.984]);
+ * var point2 = turf.point([-75.534, 39.123]);
  * var units = 'miles';
  * var distance = turf.distance(point1, point2, units);
  *
